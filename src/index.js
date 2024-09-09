9b3317b87b36d833aa362b0be4140307f5849ed3
Ahmed Maher Algohary <ahmedmaheraljwhry057@gmail.com>
2024-09-09 17:28:30 +0300

    Fixed the Routing issue

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import AppRouter from "./AppRouter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<BrowserRouter>
+	<BrowserRouter basename={process.env.PUBLIC_URL}>
 		<AppRouter />
 	</BrowserRouter>
 );
