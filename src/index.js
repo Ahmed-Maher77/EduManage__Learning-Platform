775115244f67b7aa88372b890496d802cb109fb9
Ahmed Maher Algohary <ahmedmaheraljwhry057@gmail.com>
2024-09-09 18:43:33 +0300

    Fixed the Routing issue

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import "./index.css";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./AppRouter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<HashRouter>
+	<BrowserRouter basename={process.env.PUBLIC_URL}>
 		<AppRouter />
-	</HashRouter>
+	</BrowserRouter>
 );
